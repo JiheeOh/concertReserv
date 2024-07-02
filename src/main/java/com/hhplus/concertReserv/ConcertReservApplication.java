ac42e2aa7f9fcf5264ce8a121c7644ec25fcfabd
JiheeOh <jenny2596@naver.com>
2024-07-03 00:08:27 +0900

    :tada: project init

---
+++ src/main/java/com/hhplus/concertReserv/ConcertReservApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.concertReserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertReservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertReservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhplus/concertReserv/ConcertReservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.concertReserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

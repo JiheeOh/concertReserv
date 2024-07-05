7911ed1918e0581457daeb82b0d37741f1877752
JiheeOh <jenny2596@naver.com>
2024-07-05 09:38:03 +0900

    :bulb: 예약관련 API 주석추가

--- src/main/java/com/hhplus/concertReserv/presentation/ReserveController.java
+++ src/main/java/com/hhplus/concertReserv/presentation/ReserveController.java
@@ -16,11 +16,25 @@ public class ReserveController {
         this.reserveFacade = reserveFacade;
     }
 
+    /**
+     * 토큰을 받아서 예약이 가능한 날짜와 좌석 정보를 반환한다.
+     * 토큰 안에는 유저 Id, 콘서트 Id 정보가 있다.
+     * @param authorization jwt token
+     * @return 예약가능한 날짜와 좌석 정보
+     */
     @GetMapping("/available")
     public ResponseEntity<Object> reserveAvailable(@RequestHeader String authorization) {
         return ResponseEntity.ok().body(reserveFacade.findReserveAvailable(authorization));
     }
 
+    /**
+     * 토큰,예약하려는 날짜와 좌석 정보를 받아 임시배정 처리
+     * 토큰 안에는 유저 Id, 콘서트 Id 정보가 있다.
+     * 예약하려는 날짜와 좌석 정보는 RequestBody에 있다.
+     * @param authorization jwt token
+     * @param requestBody requestBody
+     * @return 예약하려는 좌석의 가격 정보
+     */
     @PostMapping("/applySeat")
     public ResponseEntity<Object> applySeat(@RequestHeader String authorization, @RequestBody ReserveCommand.ApplySeat requestBody) {
         return ResponseEntity.ok().body(reserveFacade.applySeat(authorization,requestBody));

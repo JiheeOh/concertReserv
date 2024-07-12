f359ef822b80b6f6fc4bd5f350957706c81794ac
JiheeOh <jenny2596@naver.com>
2024-07-12 09:42:06 +0900

    :sparkles: 토큰 관련 presentation, application 계층 생성
    - Controller
    - Facade
    - Command

--- src/main/java/com/hhplus/concertReserv/application/TokenFacade.java
+++ src/main/java/com/hhplus/concertReserv/application/TokenFacade.java
@@ -1,17 +1,36 @@
 package com.hhplus.concertReserv.application;
 
 import com.hhplus.concertReserv.domain.dto.TokenDto;
+import com.hhplus.concertReserv.domain.service.ValidationService;
 import com.hhplus.concertReserv.domain.service.TokenService;
+import com.hhplus.concertReserv.exception.TokenNotFoundException;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class TokenFacade {
+    private final ValidationService validationService;
     private final TokenService tokenService;
-    public TokenFacade(TokenService tokenService){
+    public TokenFacade(ValidationService validationService, TokenService tokenService){
+        this.validationService = validationService;
         this.tokenService = tokenService;
     }
 
     public TokenDto getToken(TokenCommand.CreateToken authorization) {
-        return tokenService.getToken(authorization.memberId(),authorization.concertId());
+        UUID memberId = authorization.memberId();
+        UUID concertId = authorization.concertId();
+
+        if(!validationService.checkMemberConcertValidate(memberId,concertId)){
+            throw new IllegalArgumentException();
+        }
+        return tokenService.createToken(memberId,concertId);
+    }
+
+    public TokenDto findActivateToken(Long tokenId) {
+        if(!validationService.isToken(tokenId)){
+            throw new TokenNotFoundException("There is no token",500);
+        }
+        return tokenService.findActivateToken(tokenId);
     }
 }

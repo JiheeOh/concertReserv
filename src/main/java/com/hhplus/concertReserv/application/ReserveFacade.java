73e822ce63989036cc05f3a6d427dbaf5085ab73
JiheeOh <jenny2596@naver.com>
2024-07-19 09:32:00 +0900

    :sparkles: Interceptor 추가
    - 토큰 활성화 검증을 위해 Interceptor 추가
    - tokenservice를 주입받아 하던 검증 기능 삭제

--- src/main/java/com/hhplus/concertReserv/application/ReserveFacade.java
+++ src/main/java/com/hhplus/concertReserv/application/ReserveFacade.java
@@ -2,33 +2,25 @@ package com.hhplus.concertReserv.application;
 
 import com.hhplus.concertReserv.domain.reservation.dto.ReserveDto;
 import com.hhplus.concertReserv.domain.reservation.service.ReservationService;
-import com.hhplus.concertReserv.domain.token.service.TokenService;
-import com.hhplus.concertReserv.exception.TokenNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
 public class ReserveFacade {
-    private final TokenService tokenService;
+
     private final ReservationService reservationService;
 
-    public ReserveFacade(ReservationService reservationService, TokenService tokenService) {
+    public ReserveFacade(ReservationService reservationService) {
         this.reservationService = reservationService;
-        this.tokenService = tokenService;
+
     }
 
-    public ReserveDto findReserveAvailable(UUID concertId, Long tokenId){
-        if(tokenService.isActivateToken(tokenId)){
-            throw new TokenNotFoundException("Token is deActivated",500);
-        }
+    public ReserveDto findReserveAvailable(UUID concertId) {
         return reservationService.findReserveAvailable(concertId);
     }
 
-    public ReserveDto applySeat(ReserveCommand.ApplySeat requestBody){
-        if(tokenService.isActivateToken(requestBody.tokenId())){
-            throw new TokenNotFoundException("Token is deActivated",500);
-        }
+    public ReserveDto applySeat(ReserveCommand.ApplySeat requestBody) {
         return reservationService.applySeat(requestBody.memberId(), requestBody.seatId());
     }
 }
---
+++ src/main/java/com/hhplus/concertReserv/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package com.hhplus.concertReserv.config;
+
+import com.hhplus.concertReserv.interfaces.presentation.TokenInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new TokenInterceptor());
+    }
+}

0ff52534eb1502057435685183d7c87febaefdb3
JiheeOh <jenny2596@naver.com>
2024-07-19 09:26:27 +0900

    - Respository 구현

--- src/main/java/com/hhplus/concertReserv/infrastructure/concert/impl/ConcertRepositoryImpl.java
+++ src/main/java/com/hhplus/concertReserv/infrastructure/concert/impl/ConcertRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.hhplus.concertReserv.infrastructure.concert.impl;
 
+import com.hhplus.concertReserv.domain.concert.entity.Concert;
 import com.hhplus.concertReserv.domain.concert.repositories.ConcertRepository;
+import com.hhplus.concertReserv.infrastructure.concert.repository.ConcertJPARepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,8 +10,14 @@ import java.util.UUID;
 
 @Repository
 public class ConcertRepositoryImpl implements ConcertRepository {
-    @Override
-    public Optional<Object> findConcert(UUID concertId) {
-        return Optional.empty();
+    private final ConcertJPARepository concertJPARepository;
+
+    public ConcertRepositoryImpl(ConcertJPARepository concertJPARepository){
+        this.concertJPARepository = concertJPARepository;
     }
+    @Override
+    public Optional<Concert> findConcert(UUID concertId) {
+        return concertJPARepository.findById(concertId);
+    }
+
 }
--- src/main/java/com/hhplus/concertReserv/infrastructure/concert/repository/ConcertScheduleJPARepository.java
+++ src/main/java/com/hhplus/concertReserv/infrastructure/concert/repository/ConcertScheduleJPARepository.java
@@ -3,6 +3,12 @@ package com.hhplus.concertReserv.infrastructure.concert.repository;
 import com.hhplus.concertReserv.domain.concert.entity.ConcertSchedule;
 import com.hhplus.concertReserv.domain.concert.entity.ConcertSchedulePK;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ConcertScheduleJPARepository extends JpaRepository<ConcertSchedule, ConcertSchedulePK> {
+
+    @Query("SELECT s FROM ConcertSchedule s WHERE s.delYn= 'N'")
+    List<ConcertSchedule> findAllByDelYn();
 }

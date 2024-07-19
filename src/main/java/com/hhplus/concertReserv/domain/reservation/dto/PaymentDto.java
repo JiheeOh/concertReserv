f81f34ae9bf793bc25b48eda5d01b9e90a108adc
JiheeOh <jenny2596@naver.com>
2024-07-19 09:27:52 +0900

    :sparkles: 포인트 관련 세부 기능 추가
    - repository 구현

--- src/main/java/com/hhplus/concertReserv/domain/reservation/dto/PaymentDto.java
+++ src/main/java/com/hhplus/concertReserv/domain/reservation/dto/PaymentDto.java
@@ -11,4 +11,5 @@ import java.util.UUID;
 public class PaymentDto {
     UUID payId;
     LocalDateTime dueTime;
+    Long payAmount;
 }
--- src/main/java/com/hhplus/concertReserv/infrastructure/reservation/jpaRepository/PaymentJPARepository.java
+++ src/main/java/com/hhplus/concertReserv/infrastructure/reservation/jpaRepository/PaymentJPARepository.java
@@ -2,9 +2,13 @@ package com.hhplus.concertReserv.infrastructure.reservation.jpaRepository;
 
 import com.hhplus.concertReserv.domain.reservation.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PaymentJPARepository extends JpaRepository<Payment, UUID> {
 
+    @Query("SELECT p FROM Payment p WHERE p.dueTime > current timestamp ")
+    List<Payment> findNotPaidToken();
 }

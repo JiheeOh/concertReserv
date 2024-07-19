215229e644f27ed068473b0a0c450dceb8bbb544
JiheeOh <jenny2596@naver.com>
2024-07-19 09:25:31 +0900

    :sparkles: 콘서트 목록 조회 기능 추가

--- src/main/java/com/hhplus/concertReserv/domain/concert/service/ConcertService.java
+++ src/main/java/com/hhplus/concertReserv/domain/concert/service/ConcertService.java
@@ -2,18 +2,30 @@ package com.hhplus.concertReserv.domain.concert.service;
 
 import com.hhplus.concertReserv.domain.concert.dto.ConcertDto;
 import com.hhplus.concertReserv.domain.concert.repositories.ConcertScheduleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class ConcertService {
-    private  final ConcertScheduleRepository concertScheduleRepository;
+    private final ConcertScheduleRepository concertScheduleRepository;
 
-    public ConcertService(ConcertScheduleRepository concertScheduleRepository){
+    public ConcertService(ConcertScheduleRepository concertScheduleRepository) {
         this.concertScheduleRepository = concertScheduleRepository;
     }
+
     public List<ConcertDto> findConcertList() {
-        return concertScheduleRepository.findConcertList();
+        List<ConcertDto> result = new ArrayList<>();
+        log.info(" ==== findConcertList() start ====");
+        try{
+            result=  concertScheduleRepository.findConcertList();
+        }catch (Exception e){
+            log.error(e.toString());
+        }
+        log.info(" ==== findConcertList() end ====");
+        return result;
     }
 }

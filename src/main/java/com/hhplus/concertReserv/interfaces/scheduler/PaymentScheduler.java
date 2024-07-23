4632fbf944e1aaa46ddd91c0d07b304c922918a9
JiheeOh <jenny2596@naver.com>
2024-07-23 22:24:02 +0900

    :test_tube: 결제 기능 테스트 코드 및 정상 작동 확인

--- src/main/java/com/hhplus/concertReserv/interfaces/scheduler/PaymentScheduler.java
+++ src/main/java/com/hhplus/concertReserv/interfaces/scheduler/PaymentScheduler.java
@@ -9,10 +9,12 @@ import com.hhplus.concertReserv.domain.reservation.repositories.ReservationRepos
 import com.hhplus.concertReserv.domain.token.repositories.TokenRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Slf4j
+@Component
 public class PaymentScheduler {
 
     private final SeatRepository seatRepository;
@@ -22,7 +24,7 @@ public class PaymentScheduler {
     private final TokenRepository tokenRepository;
 
 
-    public PaymentScheduler(SeatRepository seatRepository,ReservationRepository reservationRepository,PaymentRepository paymentRepository,TokenRepository tokenRepository){
+    public PaymentScheduler(SeatRepository seatRepository, ReservationRepository reservationRepository, PaymentRepository paymentRepository, TokenRepository tokenRepository) {
         this.seatRepository = seatRepository;
         this.reservationRepository = reservationRepository;
         this.paymentRepository = paymentRepository;
@@ -31,33 +33,39 @@ public class PaymentScheduler {
     }
 
     /**
-     *
      * 스케줄러로 결제 시간 만료 처리
-     * 4분마다 작동
+     * 4초마다 작동
      * 1. 결제 마감 시간이 지난 결제Id 조회
      * 2. 결제 ID에 해당되는 token id 만료 처리
      * 3. 자리 반납 처리
      * 4. 예약 반납 처리
      */
-    @Scheduled(cron = "0 0/4 * * * *")
+    @Scheduled(cron = "0/4 * * * * *")
     private void expiredNotPaidToken() {
-        log.info("========== Expire not paid token ==========");
+        log.info("========== Expire not paid token scheduler start ==========");
 
-        // 1. 결제 마감 시간 지난 결제 ID 조회
-        List<Payment> payment = paymentRepository.findNotPaidToken();
+        try {
+            // 1. 결제 마감 시간 지난 결제 ID 조회
+            List<Payment> payment = paymentRepository.findNotPaidToken();
 
-        // 2. 토큰 만료 처리
-        List<Long> tokenIds = payment.stream().map(Payment::getTokenId).toList();
-        tokenRepository.deactivateNotPaidToken(tokenIds);
+            // 2. 토큰 만료 처리
+            List<Long> tokenIds = payment.stream().map(Payment::getTokenId).toList();
+            tokenRepository.deactivateNotPaidToken(tokenIds);
 
-        // 3. 자리 반납 처리
-        List<Reservation> reservation = payment.stream().map(Payment::getReservation).toList();
-        List<Seat> seat= reservation.stream().map(Reservation::getSeat).toList();
-        seat.forEach(x -> x.getSeatPk().setStatus(null));
-        seatRepository.updateStatus(seat);
+            // 3. 자리 반납 처리
+            List<Reservation> reservation = payment.stream().map(Payment::getReservation).toList();
+            List<Seat> seat = reservation.stream().map(Reservation::getSeat).toList();
+            seat.forEach(x -> x.getSeatPk().setStatus(null));
+            seatRepository.updateStatus(seat);
 
-        // 4. 예약 ID 삭제 처리
-        reservationRepository.deleteExpired(reservation);
+            // 4. 예약 ID 삭제 처리
+            reservationRepository.deleteExpired(reservation);
+
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+
+        log.info("========== Expire not paid token scheduler end ==========");
 
     }
 }
--- src/test/java/com/hhplus/concertReserv/domain/service/SchedulerTest.java
+++
@@ -1,7 +0,0 @@
-package com.hhplus.concertReserv.domain.service;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class SchedulerTest {
-}

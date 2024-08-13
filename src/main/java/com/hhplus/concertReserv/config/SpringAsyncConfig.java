d596fc065a66d33bc69e9d2e4a26e4b07dcf6e85
JiheeOh <jenny2596@naver.com>
2024-08-13 12:24:31 +0900

    :zap: thread Executor 설정

--- src/main/java/com/hhplus/concertReserv/config/SpringAsyncConfig.java
+++ src/main/java/com/hhplus/concertReserv/config/SpringAsyncConfig.java
@@ -15,6 +15,14 @@ public class SpringAsyncConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        // 기본적으로 유지할 스레드 수
+        threadPoolTaskExecutor.setCorePoolSize(5);
+        // 최대 허용 스레드 수
+        threadPoolTaskExecutor.setMaxPoolSize(10);
+        // 작업 큐의 크기 (대기 중인 작업 수)
+        threadPoolTaskExecutor.setQueueCapacity(500);
+        // 스레드가 유휴 상태일 때 최대 유지 시간 ( 기본 60초 )
+        threadPoolTaskExecutor.setKeepAliveSeconds(60);
         threadPoolTaskExecutor.initialize();
         return threadPoolTaskExecutor;
     }
